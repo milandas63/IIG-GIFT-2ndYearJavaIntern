e33b8c5c8e9b3936d3d8180836cd3d696ce863db
Milan Das <45796126+milandas63@users.noreply.github.com>
2023-09-05 12:12:19 +0530

    Add files via upload

---
+++ Milan Das/Day-02 (05-09-2023)/Access.java
@@ -0,0 +1,13 @@
+package com.day02;
+
+public class Access extends MTables {
+
+	public static void main(String[] args) {
+		// First way (by creating an object and calling by object reference)
+		MTables tab = new MTables();
+		System.out.println(tab.padL(25,  10));
+		
+		
+	}
+
+}
---
+++ Milan Das/Day-02 (05-09-2023)/MTables.java
@@ -0,0 +1,39 @@
+package com.day02;
+
+/*
+ * 		WAP to print all multiplication tables between
+ * 		2 to 25?
+ */
+
+public class MTables {
+	public static void main(String[] args) {
+		int start = 2;
+		int end = 25;
+
+		for(int i=start; i<=end; i++) {
+			for(int j=1; j<=10; j++) {
+				System.out.println(i + " x " + j + " = " + (i*j));
+			}
+			System.out.println();
+		}
+
+
+		for(int h=start; h<=end; h+=6) {
+			for(int i=1; i<=10; i++) {
+				for(int j=h; j<=(h+5); j++) {
+					System.out.print(padL(j,2) + " x " + padL(i,2) + " = " + padL((i*j),3) + "  ");
+				}
+				System.out.println();
+			}
+			System.out.println();
+		}
+	}
+	
+	public static String padL(int n, int l) {
+		String buf = ""+n;
+		for(int i=buf.length(); i<l; i++) {
+			buf = " "+buf;
+		}
+		return buf;
+	}
+}
