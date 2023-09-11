4f3e54f4635b27a74801d60305735e133513331a
Milan Das <45796126+milandas63@users.noreply.github.com>
2023-09-11 12:08:37 +0530

    Add files via upload

---
+++ Milan Das/Day-07 (11-09-2023)/CommandLineData.java
@@ -0,0 +1,11 @@
+package com.day07;
+
+public class CommandLineData {
+
+	public static void main(String[] args) {
+		for(int i=0; i<args.length; i++) {
+			System.out.println(args[i]);
+		}
+	}
+
+}
