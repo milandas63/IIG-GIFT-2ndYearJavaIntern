d553ae04ee83a7214f16c53484ab7ce40fbc52a6
Harapriya-Das <143993361+Harapriya-Das@users.noreply.github.com>
2023-09-12 22:24:29 +0530

    Create diamond_asteriks.java

---
+++ Assignment/Harapriya Das/diamond_asteriks.java
@@ -0,0 +1,74 @@
+/*Draw a diamond asteriks */
+package com.day1;
+import java.util.Scanner;
+public class diamond_asteriks
+{
+ 
+public static void main(String[] args)
+{
+ 
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter N : ");
+int n=sc.nextInt(); 
+System.out.print("Enter Symbol : ");
+ 
+char c = sc.next().charAt(0);
+ 
+for(int i=1;i<=n;i++)
+{
+for(int j=1;j<=n-i;j++)
+ 
+{
+System.out.print(" ");
+}
+ 
+for(int j=1;j<=i*2-1;j++)
+ 
+{
+System.out.print(c);
+}
+System.out.println();
+ 
+} 
+for(int i=n-1;i>0;i--)
+{
+for(int j=1;j<=n-i;j++)
+ 
+{
+System.out.print(" ");
+}
+for(int j=1;j<=i*2-1;j++)
+ 
+{
+System.out.print(c);
+}
+System.out.println();
+}
+ 
+}
+}
+
+/*OUTPUT:
+Enter N : 
+10
+Enter Symbol : *
+         *
+        ***
+       *****
+      *******
+     *********
+    ***********
+   *************
+  ***************
+ *****************
+*******************
+ *****************
+  ***************
+   *************
+    ***********
+     *********
+      *******
+       *****
+        ***
+         *
+  */
