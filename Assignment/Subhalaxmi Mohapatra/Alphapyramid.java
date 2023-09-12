3df8736192e209473a5d3529064310f42a2e34dc
Subhalaxmi-29 <143996004+Subhalaxmi-29@users.noreply.github.com>
2023-09-12 21:43:12 +0530

    Create Alphapyramid.java

---
+++ Assignment/Subhalaxmi Mohapatra/Alphapyramid.java
@@ -0,0 +1,26 @@
+package com.day3;
+public class AlphaPyramid {
+
+	public static void main(String[] args) {
+	int s = 35;
+		    for (int i=97;i<=107;i++) {
+			for (int j=0;j<s;j++) System.out.print(" ");
+			for (int j=97;j<i;j++)System.out.print((char)j);
+			for (int j=i-2;j>=97;j--)System.out.print((char)j);
+			System.out.println();
+			s--;
+		}
+	}
+
+}
+/*OUTPUT:
+                                  a
+                                 aba
+                                abcba
+                               abcdcba
+                              abcdedcba
+                             abcdefedcba
+                            abcdefgfedcba
+                           abcdefghgfedcba
+                          abcdefghihgfedcba
+                         abcdefghijihgfedcba
