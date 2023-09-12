7da2e7d3a21678a568dcc679845c2ab40170c90d
Subhalaxmi-29 <143996004+Subhalaxmi-29@users.noreply.github.com>
2023-09-12 21:05:59 +0530

    Create Name_Greet.java

---
+++ Assignment/Subhalaxmi Mohapatra/Name_Greet.java
@@ -0,0 +1,17 @@
+package com.day1;
+import java.util.Scanner;
+public class Name_Greet {
+public static void main(String[] args) {
+Scanner sc= new Scanner(System.in);
+		        System.out.print("Enter your name!: ");
+		        String s=sc.next();
+		        System.out.println("Good Morning!"+", "+s);
+		        sc.close();
+		    }
+		}
+		  
+
+	
+/*OUTPUT:
+  Enter your name!: Sir
+Good Morning!, Sir
