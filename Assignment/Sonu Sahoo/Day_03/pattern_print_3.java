e396d7ce2b1ecb23da4d42955f4613c3ba4e447d
sonu07777 <122894139+sonu07777@users.noreply.github.com>
2023-09-12 20:20:51 +0530

    Create pattern_print_3.java

---
+++ Assignment/Sonu Sahoo/Day_03/pattern_print_3.java
@@ -0,0 +1,45 @@
+// Question 3.
+package com.Day_3;
+  public class question3 {
+    public static void main(String[] args) {
+        int space =15;
+        int limit=1;
+        int line;
+        for( line=1; line <=8;line++){
+            for(int i=1;i<=line;i++){
+                System.out.print(i);
+            }
+            for(int j=1; j<=space;j++){
+                System.out.print(" ");
+            }
+
+            for(int k=line;k>0;k--){
+                System.out.print(k);
+            }
+            limit=limit+1;
+            space=space-2;  
+            System.out.println();
+        }
+        for(int i=1;i<=9;i++)
+        {
+            System.out.print(i);
+        }
+        for(int i=8;i>=1;i--)
+        {
+            System.out.print(i);
+        }
+    }
+}
+    
+
+// output:-
+
+// 1               1
+// 12             21
+// 123           321
+// 1234         4321
+// 12345       54321
+// 123456     654321
+// 1234567   7654321
+// 12345678 87654321
+// 12345678987654321
