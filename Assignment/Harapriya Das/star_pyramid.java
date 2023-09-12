e0526b0847ed5c5f8f1518858cd45547d5b86910
Harapriya-Das <143993361+Harapriya-Das@users.noreply.github.com>
2023-09-12 21:34:54 +0530

    Create star_pyramid.java

---
+++ Assignment/Harapriya Das/star_pyramid.java
@@ -0,0 +1,57 @@
+/*Write a program to print a pyramid in the following manner>?
+
+         *          
+        * *         
+       *   *        
+      *     *       
+     *       *      
+    *         *     
+   *           *    
+  *             *   
+ *               *  
+*******************  */
+
+package com.day1;
+
+public class star_pyramid {
+    public static void main(String[] args) {
+        int numRows = 10; 
+        
+        printHollowPyramid(numRows);
+    }
+
+    public static void printHollowPyramid(int numRows) {
+        for (int i = 1; i <= numRows; i++) {
+            
+            for (int j = 1; j <= numRows - i; j++) {
+                System.out.print(" ");
+            }
+
+            
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                if (k == 1 || k == 2 * i - 1 || i == numRows) {
+                    
+                    System.out.print("*");
+                } else {
+                    
+                    System.out.print(" ");
+                }
+            }
+
+            
+            System.out.println();
+        }
+    }
+}
+
+ /* OUTPUT
+         *          
+        * *         
+       *   *        
+      *     *       
+     *       *      
+    *         *     
+   *           *    
+  *             *   
+ *               *  
+*******************  */
