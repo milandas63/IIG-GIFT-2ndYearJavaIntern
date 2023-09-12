ed7e9cb2cffa34741f22515473e4bab0aa8131a7
Harapriya-Das <143993361+Harapriya-Das@users.noreply.github.com>
2023-09-12 22:00:28 +0530

    Create asteriks_pyramid.java

---
+++ Assignment/Harapriya Das/asteriks_pyramid.java
@@ -0,0 +1,34 @@
+package com.day1;
+
+public class asteriks_pyramid {
+
+	public static void main(String[] args) {
+		
+		        int sizeOfPyramid = 8;
+		         
+		        for(int i=1;i<=sizeOfPyramid;i++) {
+		      
+		            for(int j=i;j<=sizeOfPyramid-1;j++) {
+		                System.out.print(" ");
+		            }
+		       
+		     
+		            for(int k=1;k<=2*i-1;k++){
+		                System.out.print("*");
+		            }
+		            System.out.println("");
+		        }
+	}
+
+}
+
+/*OUTPUT:
+       *
+      ***
+     *****
+    *******
+   *********
+  ***********
+ *************
+***************
+*/
