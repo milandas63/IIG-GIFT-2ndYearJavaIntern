7248f2c63754d0d3aabab6a7b8f1a34195894665
Harapriya-Das <143993361+Harapriya-Das@users.noreply.github.com>
2023-09-13 21:48:32 +0530

    Create ExeptionHandling1.java

---
+++ Assignment/Harapriya Das/Day-7/ExeptionHandling1.java
@@ -0,0 +1,33 @@
+package com.day07;
+
+public class ExceptionHandling1 {
+
+	public static void main(String[] args) {
+		int n = 125;
+		int d = 0;
+		
+		try {
+			double r = n/d;
+			System.out.println("There is no exception");
+			System.out.println("Other lines");
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("ArrayIndexOutOfBoundsException");
+		} catch(ClassCastException e) {
+			System.out.println("ClassCastException");
+		} catch(ArithmeticException e) {
+			System.out.println("ArithmeticException");
+		} catch(IllegalArgumentException e) {
+			System.out.println("IllegalArgumentException");
+		}
+
+	}
+
+}
+
+class MyException extends Throwable {
+	
+}
+
+/*OUTPUT:
+ArithmeticException
+*/
