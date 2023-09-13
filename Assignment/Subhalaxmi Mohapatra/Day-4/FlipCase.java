0897099d52413ef3596c9c972b4e6b18090cd043
Subhalaxmi-29 <143996004+Subhalaxmi-29@users.noreply.github.com>
2023-09-13 21:33:48 +0530

    Create FlipCase.java

---
+++ Assignment/Subhalaxmi Mohapatra/Day-4/FlipCase.java
@@ -0,0 +1,22 @@
+package com.day4;
+
+public class FlipCase {
+
+	public static void main(String[] args) {
+String text = "Quick Brown Fox Jumps Over The Lazy Dog";
+StringBuffer buf = new StringBuffer();
+char c;
+for(int i=0;i<text.length();i++) {
+	c=text.charAt(i);
+	if (c>=65 && c<=90){
+		c=(char)(c+32);
+		} else if (c>=97 && c<=122){
+			c=(char)(c-32);
+		}
+		buf.append(c);
+	}
+System.out.println(buf.toString());	
+}
+}
+OUTPUT:
+qUICK bROWN fOX jUMPS oVER tHE lAZY dOG
