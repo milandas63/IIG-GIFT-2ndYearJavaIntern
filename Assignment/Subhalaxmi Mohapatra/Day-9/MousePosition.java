efe421bc27b6a042206d8198d61a6c0f24ff9391
Subhalaxmi-29 <143996004+Subhalaxmi-29@users.noreply.github.com>
2023-09-13 22:26:43 +0530

    Create MousePosition.java

---
+++ Assignment/Subhalaxmi Mohapatra/Day-9/MousePosition.java
@@ -0,0 +1,40 @@
+package com.day9;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import javax.swing.JFrame;
+public class MousePosition extends JFrame {
+	public MousePosition() {
+		super("Mouse Location");
+        this.addMouseMotionListener(new MotionClass());
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setBounds(100,75,1200,650);
+		this.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new MousePosition();
+	}
+}
+class MotionClass implements MouseMotionListener {
+	int x;
+	int y;
+	String pos;
+
+	@Override
+	public void mouseDragged(MouseEvent me) {
+		x = me.getX();
+		y = me.getY();
+		pos = x+"/"+y;
+		me.getComponent().getGraphics().clearRect(x-10, y-10, 70, 70);
+		me.getComponent().getGraphics().drawString(pos, x, y);
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent me) {
+		x = me.getX();
+		y = me.getY();
+		pos = x+"/"+y;
+		me.getComponent().getGraphics().clearRect(x-10, y-10, 70, 70);
+		me.getComponent().getGraphics().drawString(pos, x, y);
+	}
+}
