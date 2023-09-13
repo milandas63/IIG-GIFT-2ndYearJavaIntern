5e63505086257c96cbff9656b2a0d277288d4564
Jayashree-kar <143938142+Jayashree-kar@users.noreply.github.com>
2023-09-13 10:54:59 +0530

    Add files via upload

---
+++ Assignment/Jayshree Kar/assignment3.java
@@ -0,0 +1,20 @@
+public class assignment3 {
+
+	public static void main(String[] args) {
+		int s = 35;
+		for(int i=97; i<=107; i++) {
+			for(int j=0; j<s; j++) System.out.print(" ");
+			for(int j=97; j<i; j++) System.out.print((char)j);
+			for(int j=i-2; j>=97; j--) System.out.print((char)j);
+			System.out.println();
+			s--;
+		}
+		System.out.println();
+		int n = new Integer(25);
+		Integer in = 25;
+		long number = 2534533443332334L;
+		float deci = 33.45F;
+		
+	}
+
+}
