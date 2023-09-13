0539cb826f76f7400dae0f22260ff1863b9a04b7
Nihareeka28 <143897332+Nihareeka28@users.noreply.github.com>
2023-09-13 11:03:53 +0530

    Add files via upload

---
+++ Assignment/Nihareeka Meher/assignment4.java
@@ -0,0 +1,25 @@
+public class assignment4 {
+
+	public static void main(String[] args) {
+		String text ="Java is an Object Oriented Programming language";
+		char[] vowels = {'A','E','I','O','U','a','e','i','o','u'};
+		char c;
+		int count=0;
+		for(int i=0;i<text.length();i++)
+		{
+			c=text.charAt(i);
+			for(int j=0;j<vowels.length;j++) {
+				if(c==vowels[j])
+				{
+					count++;
+					break;
+				}
+				
+			}
+			
+		}
+		System.out.println("Total vowels="+count);
+
+	}
+
+}
