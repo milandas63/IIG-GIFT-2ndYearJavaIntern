f6f9d8ef1f7e15584c7a2914b2e0c58fc8dfea1b
Somasterisk011 <143945523+Somasterisk011@users.noreply.github.com>
2023-09-14 22:31:28 +0530

    Create MTable.java

---
+++ Assignment/Somanath Prusty/Day-02(05-09-2023)/MTable.java
@@ -0,0 +1,22 @@
+
+public class MTable {
+
+	public static void main(String[] args) {
+		int start=2;
+		int end=25;
+		for(int k=start; k<=end; k+=6) {
+			for(int i=1; i<=10; i++) {
+				for(int j=k; j<=(k+5); j++) {
+					System.out.print(padl(j,2) + " x " + padl(i,2) + " = " + padl((i*j),3)+"  ");
+				}System.out.println();
+			}System.out.println();
+		}
+	}
+	public static String padl(int n, int l) {
+		String b=""+n;
+		for(int i=b.length(); i<l; i++) {
+			b=" "+b;
+		}return b;
+	}
+
+}
