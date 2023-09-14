59108734963a522f709634438a060d63459171b7
Suvra12345 <144014918+Suvra12345@users.noreply.github.com>
2023-09-14 11:14:57 +0530

    Create Output.java

---
+++ Assignment/Suvra Nayak/Day9/Output.java
@@ -0,0 +1,13 @@
+package com.day9;
+import java.util.Scanner;
+public class Output {
+public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Username: ");
+		String user = sc.nextLine();
+		System.out.println("Hello "+user);
+	}
+}
+OUTPUT:
+Username: suvra
+Hello suvra
