862d30cc4f7fc67c739a9107f92c28c866824560
Suvra12345 <144014918+Suvra12345@users.noreply.github.com>
2023-09-14 10:24:52 +0530

    Create VowelCount.java

---
+++ Assignment/Suvra Nayak/Day4/VowelCount.java
@@ -0,0 +1,23 @@
+package com.day4;
+public class VowelCount {
+public static void main(String[] args) {
+		String text = "I am a student of GIFT college";
+		char[] vowels = {'A','E','I','O','U','a','e','i','o','u'};
+		
+		char c;
+		int count = 0;
+		for(int i=0; i<text.length(); i++) {
+			c = text.charAt(i);
+			for(int j=0; j<vowels.length; j++) {
+				if(c==vowels[j]) {
+					count++;
+					break;
+				}
+			}
+		}
+		System.out.println("Total vowels = "+count);
+	}
+
+}
+OUTPUT:
+Total vowels = 10
