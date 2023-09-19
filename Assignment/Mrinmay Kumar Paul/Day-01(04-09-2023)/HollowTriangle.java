c56460a5fbc38a5e9feca0f18541b622d9af8e37
Mrinmay Kumar Paul <144090352+Mrinmay430143@users.noreply.github.com>
2023-09-19 20:25:09 +0530

    Create HollowTriangle.java

---
+++ Assignment/Mrinmay Kumar Paul/Day-01(04-09-2023)/HollowTriangle.java
@@ -0,0 +1,42 @@
+/*Write a program to print a pyramid in the following manner>?
+
+         *          
+        * *         
+       *   *        
+      *     *       
+     *       *      
+    *         *     
+   *           *    
+  *             *   
+ *               *  
+******************* 
+/* */
+
+public class HollowTriangle {
+
+	public static void main(String[] args) {
+	
+		 int spaces = 20;
+		 int stars=1;
+		    for(int line=0; line<10; line++){
+		        for(int i=0; i<spaces; i++) System.out.print(" ");
+		        for(int i=0; i<stars; i++) {
+		            if(line < 9){
+		            	if(i==0 || i==stars-1){
+		            		System.out.print("*");
+		            	}else{
+		            		System.out.print(" ");
+		            	}
+		            }else{
+		            	System.out.print("*");
+		            }
+		        }
+		        System.out.println();
+		        spaces -= 1;
+		        stars += 2;
+		    }
+
+		    	System.out.println();
+
+	}
+}
