473d4ea8365c4aaeee7fea1c5b98c6137f0f502f
rashmiranjan9178 <125286238+rashmiranjan9178@users.noreply.github.com>
2023-09-25 12:35:39 +0530

    Update Assignment_MySQL.java

--- Assignment/Rasmi Ranjan Panda/Assignment_MySQL.java
+++ Assignment/Rasmi Ranjan Panda/Assignment_MySQL.java
@@ -13,26 +13,26 @@ public class Assignment_MySQL {
 	
 	public Assignment_MySQL() {
 		try {
-			// Step-1: Load and register driver
+			
 			Class.forName("org.gjt.mm.mysql.Driver");
 	
-			// Step-2: Create Connection object
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/gift_4","root","");
+			
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/intern","root","");
 
-			// Step-3: Create a Statement object
+		
 			stmt = conn.createStatement();
 			
-			// Step-4: Create a ResultSet object
+			
 			rs = stmt.executeQuery("SELECT * FROM colleges");
 			
-			// Step-5: Manipulate ResultSet
+			
 			while(rs.next()) {
 				System.out.println(rs.getInt(1)+"  "+
 			                       rs.getString(2)+"  "+
 						           rs.getInt(3));
 			}
 			
-			// Step-6: Close ResultSet, Statement, Connection
+			
 			rs.close();
 			stmt.close();
 			conn.close();
