a242f9140813335fb691f36cc48fcdcfe4ea528e
Aarti178 <143997909+Aarti178@users.noreply.github.com>
2023-09-25 21:13:34 +0530

    Create piramid.java

---
+++ Assignment/Arti Kumari/piramid.java
@@ -0,0 +1,45 @@
+package com.day1;
+
+public class pyramid {
+    public static void main(String[] args) {
+        int numRows = 10; 
+        
+        printHollowPyramid(numRows);
+    }
+
+    public static void printHollowPyramid(int numRows) {
+        for (int i = 1; i <= numRows; i++) {
+            
+            for (int j = 1; j <= numRows - i; j++) {
+                System.out.print(" ");
+            }
+
+            
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                if (k == 1 || k == 2 * i - 1 || i == numRows) {
+                    
+                    System.out.print("*");
+                } else {
+                    
+                    System.out.print(" ");
+                }
+            }
+
+            
+            System.out.println();
+        }
+    }
+
+
+/*ouput
+         *
+        * *
+       *   *
+      *     *
+     *       *
+    *         *
+   *           *
+  *             *
+ *               *
+*******************
+*/
