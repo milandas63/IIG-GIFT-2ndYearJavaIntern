9968078a43b24f94d961139202930b5b3bdf025a
rashmiranjan9178 <125286238+rashmiranjan9178@users.noreply.github.com>
2023-09-25 12:38:50 +0530

    Update assignment8.java

--- Assignment/Rasmi Ranjan Panda/assignment8.java
+++ Assignment/Rasmi Ranjan Panda/assignment8.java
@@ -5,23 +5,20 @@ import java.io.IOException;
 
 public class AddContentToFile {
     public static void main(String[] args) {
-        String fileName = "Day8/example.txt"; // Specify the name of your file
+        String fileName = "myProgram.txt";
 
         try {
-            // Create a FileWriter with the specified file name and set append mode to true
+            
             FileWriter fileWriter = new FileWriter(fileName, true);
             
-            // Create a BufferedWriter for efficient writing
+            
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
-            // Content to be added to the file
             String contentToAdd = "This is the new content that will be added to the file.";
 
-            // Write the content to the file
             bufferedWriter.write(contentToAdd);
-            bufferedWriter.newLine(); // Add a newline character if needed
+            bufferedWriter.newLine(); 
 
-            // Close the buffered writer
             bufferedWriter.close();
 
             System.out.println("Content added to the file successfully.");
