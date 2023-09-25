51bb4496da3a40629514a6205788abada2a39ab8
rashmiranjan9178 <125286238+rashmiranjan9178@users.noreply.github.com>
2023-09-25 12:34:06 +0530

    Add files via upload

---
+++ Assignment/Rasmi Ranjan Panda/Assignment_MySQL.java
@@ -0,0 +1,51 @@
+package Day13;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Assignment_MySQL {
+	Connection conn;
+	Statement stmt;
+	ResultSet rs;
+	
+	public Assignment_MySQL() {
+		try {
+			// Step-1: Load and register driver
+			Class.forName("org.gjt.mm.mysql.Driver");
+	
+			// Step-2: Create Connection object
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/gift_4","root","");
+
+			// Step-3: Create a Statement object
+			stmt = conn.createStatement();
+			
+			// Step-4: Create a ResultSet object
+			rs = stmt.executeQuery("SELECT * FROM colleges");
+			
+			// Step-5: Manipulate ResultSet
+			while(rs.next()) {
+				System.out.println(rs.getInt(1)+"  "+
+			                       rs.getString(2)+"  "+
+						           rs.getInt(3));
+			}
+			
+			// Step-6: Close ResultSet, Statement, Connection
+			rs.close();
+			stmt.close();
+			conn.close();
+			
+		} catch(ClassNotFoundException e) {
+			System.out.println(e);
+		} catch(SQLException e) {
+			System.out.println(e);
+		}
+	}
+
+	public static void main(String[] args) {
+		new Assignment_MySQL();
+	}
+
+}
